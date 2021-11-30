11ec4dad935c85dc631630c36d40033e817cc26c
gpavlikov <gpavlikov@sdvor.com>
2021-11-30 19:02:57 +0500

    Заложил основу модели "Объявление"

--- advts/models.py
+++ advts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Advert(models.Model):
+
+    title = models.CharField(max_length=128, verbose_name='Заголовок объявления')
+    description = models.TextField(verbose_name='Текст объявления')
+    price = models.DecimalField(max_digits=12, decimal_places=2, verbose_name='Цена')
+    address = models.CharField(max_length=128, verbose_name='Адрес')
+
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата и вермя создания")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата и время последнего обновления')
+
+    def __str__(self):
+        return self.title + ' ' + str(self.price)
